0427da0fc3bd5130b78e77f70c33fc910fe810c7
Romain H <94904431+LePandadu78@users.noreply.github.com>
2022-08-23 11:38:37 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const port = 5000;
 
 const welcome = (req, res) => {
@@ -14,7 +16,7 @@ app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
 
-const usersHandlers = require("./userHandlers");
+const usersHandlers = require("./usersHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
@@ -30,3 +32,7 @@ app.listen(port, (err) => {
     console.log(`Server is listening on ${port}`);
   }
 });
+
+app.post("/api/movies", movieHandlers.postMovie);
+
+app.post("/api/users", usersHandlers.postUsers);
---
+++ usersHandlers.js
@@ -0,0 +1,56 @@
+const database = require("./database");
+
+
+const getUsers = (req, res) => {
+  console.log(req)
+  database
+  .query("select * from users")
+  .then(([users]) => {
+    res.json(users);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send("Error retrieving data from database");
+  });
+};
+
+const getUsersById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("select * from users where id = ?", [id])
+    .then(([users]) => {
+      if (users[0] != null) {
+        res.json(users[0]);
+      } else {
+        res.status(404).send("Not Found");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
+const postUsers = (req, res) => {
+    const { firstname, lastname, email } = req.body;
+  
+    database
+      .query(
+        "INSERT INTO users(firstname, lastname, email) VALUES (?, ?, ?)",
+        [firstname, lastname, email]
+      )
+      .then(([result]) => {
+        res.location(`/api/users/${result.insertId}`).sendStatus(201);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error saving user");
+      });
+  };
+
+module.exports = {
+  getUsers,
+  getUsersById,
+  postUsers,
+};
